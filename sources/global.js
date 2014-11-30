b1350b06aa9ff02096ce0fb17c63dbf2ac0e09af
Alexei Skachykhin <alexei.skachykhin@live.com>
2014-11-30 23:32:57 +0300

    Replaced eval with algorithmic (safer) method of expression resolution, that mimics standardized Reference resolution mechanism.

--- sources/global.js
+++ sources/global.js
@@ -1,13 +1,67 @@
 define(function () {
     'use strict';
 
+    // Calling eval indirectly, to obtain global object (http://ecma-international.org/ecma-262/5.1/#sec-10.4.2).
+    // We are making this code portable across different execution environments by not having direct reference to 'window'.
+    var global = (0, eval)('this');
+
+
+    function resolve(expression) {
+        var referenceTarget;
+        var propertyNames = expression.split('.');
+        
+        for (var propertyIndex = 0;
+            propertyIndex < propertyNames.length;
+            propertyIndex++) {
+            
+            var propertyName = propertyNames[propertyIndex];
+            referenceTarget = resolveReference(referenceTarget, propertyName, propertyIndex);
+        }
+
+        return referenceTarget;
+    }
+
+
+    function resolveReference(referenceTarget, propertyName, propertyIndex) {
+        var referenceBase = getReferenceBase(referenceTarget, propertyName, propertyIndex);
+
+        if (isUnresolvableReference(referenceBase)) {
+            if (propertyIndex === 0) {
+                throw new ReferenceError('Require Global: ' + propertyName + ' is not defined.');
+            }
+            else {
+                throw new TypeError('Require Global: Can\'t read ' + propertyName + ' of ' + referenceTarget + '.');
+            }
+        }
+
+        return referenceBase[propertyName];
+    }
+
+    function getReferenceBase(referenceTarget, propertyName, propertyIndex) {
+        if (propertyIndex === 0) {
+            // Emulate behavior of unresolvable references (http://ecma-international.org/ecma-262/5.1/#sec-8.7),
+            // thus minimazing impact on semantics of a program when swithing from global variables to Require Global.
+            if (propertyName in global) {
+                return global;
+            }
+
+            return;
+        }
+
+        return referenceTarget;
+    }
+
+    function isUnresolvableReference(referenceBase) {
+        return (referenceBase == null);
+    }
+
 
     function load(name, localRequire, onLoad, config) {
         if (config.isBuild) {
             onLoad(null);
         }
         else {
-            var value = eval(name);
+            var value = resolve(name);
             onLoad(value);
         }
     }
